2940b418bf5fe18c685f9c5df664bd4c8325a4fe
Anna Zahrebelna <annzagrebelnaya@gmail.com>
2020-06-01 22:01:50 +0300

    part2 week2

---
+++ part2/src/main/java/courses/algorithms/part2/Week2.java
@@ -0,0 +1,97 @@
+package courses.algorithms.part2;
+
+import com.sun.tools.javac.util.Pair;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.Arrays.asList;
+
+public class Week2 {
+
+    private static final int SOURCE_NODE = 1;
+    private static final List<Integer> VERTICES_INCLUDED_IN_SMALL_RESULT = asList(1,3);
+    private static final List<Integer> VERTICES_INCLUDED_IN_RESULT = asList(7,37,59,82,99,115,133,165,188,197);
+
+    public static class SmallExample {
+        public static void main(String[] args) throws IOException, URISyntaxException {
+            Map<Integer, Map<Integer, Integer>> graph = readUndirectedWeightedGraph("week2/SmallUndirectedWeightedGraph.txt");
+            Map<Integer,Integer> shortestPaths = dijkstra(SOURCE_NODE, graph);
+            List<Integer> result = VERTICES_INCLUDED_IN_SMALL_RESULT.stream()
+                    .map(shortestPaths::get)
+                    .collect(Collectors.toList());
+            System.out.println(shortestPaths);
+            System.out.println(result);
+        }
+    }
+
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        Map<Integer, Map<Integer, Integer>> graph = readUndirectedWeightedGraph("week2/BigUndirectedWeightedGraph.txt");
+        Map<Integer,Integer> shortestPaths = dijkstra(SOURCE_NODE, graph);
+        List<Integer> result = VERTICES_INCLUDED_IN_RESULT.stream()
+                .map(shortestPaths::get)
+                .collect(Collectors.toList());
+        System.out.println(result);
+    }
+
+    private static Map<Integer, Integer> dijkstra(int source, Map<Integer, Map<Integer, Integer>> graph) {
+        Set<Integer> unexploredNodes = new HashSet<>(graph.keySet());
+        unexploredNodes.remove(source);
+        Set<Integer> exploredNodes = new HashSet<>();
+        exploredNodes.add(source);
+
+        Map<Integer, Integer> scores = new HashMap<>();
+        scores.put(source, 0);
+
+        while (!unexploredNodes.isEmpty()) {
+            Integer minScore = null;
+            Integer minNode = null;
+            //define edges (i,j) where i is explored, j is not explored
+            for (int i: exploredNodes) {
+                Integer iScore = scores.get(i);
+                for (Map.Entry<Integer, Integer> jAndWeight: graph.get(i).entrySet()) {
+                    if (!unexploredNodes.contains(jAndWeight.getKey())) {
+                        continue;
+                    }
+                    int currentScore = iScore + jAndWeight.getValue();
+                    //pick one that minimizes scores[i] + w
+                    if (minScore == null || currentScore < minScore) {
+                        minScore = currentScore;
+                        minNode = jAndWeight.getKey();
+                    }
+                }
+            }
+            //add it to explored, update scores
+            exploredNodes.add(minNode);
+            unexploredNodes.remove(minNode);
+            scores.put(minNode, minScore);
+        }
+        return scores;
+    }
+
+    private static Map<Integer, Map<Integer, Integer>> readUndirectedWeightedGraph(String fileName) throws IOException, URISyntaxException {
+        Path path = Paths.get(ClassLoader.getSystemResource(fileName).toURI());
+        return Files.readAllLines(path)
+                .stream()
+                .map(s -> Arrays.stream(s.split("\t"))
+                        .collect(Collectors.toList())
+                )
+                .collect(Collectors.toMap(list -> Integer.parseInt(list.get(0)),
+                        list -> list.stream()
+                                .filter(s -> s.contains(","))
+                                .map(s -> {
+                                    String[] nodeAndWeight = s.split(",");
+                                    return Pair.of(
+                                            Integer.parseInt(nodeAndWeight[0]),
+                                            Integer.parseInt(nodeAndWeight[1])
+                                            );
+                                })
+                        .collect(Collectors.toMap(pair -> pair.fst, pair -> pair.snd))
+                ));
+    }
+}
